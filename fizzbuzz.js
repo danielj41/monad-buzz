e5a583f83e8c786df02f3500d52f0bbc8cf4a7f8
Daniel Johnson <danielj41@gmail.com>
2017-07-16 20:28:04 -0700

    use io monad (clean up later)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,33 +1,50 @@
 // monad variants
 
-class Lifted {
+// io
+class Truth {
+  constructor(speakTheTruth) {
+    this.speakTheTruth = speakTheTruth;
+  }
+
+  bind(nextSpeaker) {
+    return new Truth(w => {
+      const { words, value } = this.speakTheTruth(w);
+      return nextSpeaker(words, value).speakTheTruth(words);
+    });
+  }
+}
+
+function lifted(v) {
+  return new Truth((words, value) => {return { words, value: new Yes(v) }});
+}
+
+function spoken(moreWords) {
+  return new Truth((words, value) => {return { words: words + moreWords, value: new No() }});
+}
+
+function pass() {
+  return new Truth((words, value) => {return { words, value: new No() }});
+}
+
+function consoleTruth(truth) {
+  console.log(truth.speakTheTruth('').words);
+}
+
+
+
+class Yes {
   constructor(value) {
     this.value = value;
   }
 
-  chain(transform) {
-    var result = transform(this.value);
-    return result instanceof RideItOut ? this : result;
+  bind(fn) {
+    return fn(this.value);
   }
 }
 
-class Descend {
-  constructor(text) {
-    this.text = text;
-  }
-
-  chain(transform) {
-    return this;
-  }
-
-  toJSON() {
-    return this.text;
-  }
-}
-
-class RideItOut {
-  chain(transform) {
-    return this;
+class No {
+  bind(fn) {
+    return pass();
   }
 }
 
@@ -55,32 +72,35 @@ function* range(start, end) {
 
 // fizzbuzz computations
 
-function fizzbuzzComp(value) {
-  if (value % 5 === 0 && value % 3 === 0) {
-    return new Descend("fizzbuzz");
-  }
-
-  return new RideItOut();
+function fizzbuzzComp(words, maybe) {
+  return maybe.bind(value => {
+    if (value % 5 === 0 && value % 3 === 0) {
+      return spoken("fizzbuzz");
+    }
+    return lifted(value);
+  });
 }
 
-function fizzComp(value) {
-  if (value % 3 === 0) {
-    return new Descend("fizz");
-  }
-
-  return new RideItOut();
+function fizzComp(words, maybe) {
+  return maybe.bind(value => {
+    if (value % 3 === 0) {
+      return spoken("fizz");
+    }
+    return lifted(value);
+  });
 }
 
-function buzzComp(value) {
-  if (value % 5 === 0) {
-    return new Descend("buzz");
-  }
-
-  return new RideItOut();
+function buzzComp(words, maybe) {
+  return maybe.bind(value => {
+    if (value % 5 === 0) {
+      return spoken("buzz");
+    }
+    return lifted(value);
+  });
 }
 
-function idComp(value) {
-  return new Descend(value);
+function idComp(words, maybe) {
+  return maybe.bind(spoken);
 }
 
 
@@ -88,18 +108,17 @@ function idComp(value) {
 // fizzbuzz implementation
 
 function LazyFizzbuzzerFactory() {
-  return chainGenerator(range(1, 100), (value) => new Lifted(value)
-      .chain(fizzbuzzComp)
-      .chain(fizzComp)
-      .chain(buzzComp)
-      .chain(idComp)
-      .toJSON());
+  return chainGenerator(range(1, 100), value => lifted(value)
+      .bind(fizzbuzzComp)
+      .bind(fizzComp)
+      .bind(buzzComp)
+      .bind(idComp));
 }
 
 
 
 // main
 
-for (text of LazyFizzbuzzerFactory()) {
-  console.log(text + "\n");
+for (truth of LazyFizzbuzzerFactory()) {
+  consoleTruth(truth);
 }
