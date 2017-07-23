034dbe486d89a2bf127aa3de1166aff1095fb5cb
Daniel Johnson <danielj41@gmail.com>
2017-07-22 18:53:15 -0700

    fizzbuzz, generator-utils: add function doc comments

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,6 +2,12 @@ import { chainTruthGenerator, range } from './generator-utils.js';
 import { fizzbuzzComp, fizzComp, buzzComp, idComp } from './fizzbuzz-comps.js';
 import { pass } from './monad-utils.js';
 
+/**
+ * @return Iterator A lazy iterator that will perform one computation per
+ *                  iteration. It will return the current `Truth` at each
+ *                  iteration, which can be evaluated at the end of the program
+ *                  to create the final output.
+ */
 export default function fizzbuzzer() {
   return chainTruthGenerator(pass(), range(1, 100), truth => truth
       .bind(fizzbuzzComp)
--- src/generator-utils.js
+++ src/generator-utils.js
@@ -1,11 +1,20 @@
 import { lifted, spoken } from './monad-utils.js';
 
+/**
+ * Maps each value from `generator` through `transform`, chaining IO operations
+ * together.
+ *
+ * @param Truth truth The initial IO monad to bind through the computations.
+ * @param Iterator generator An iterator of values to `transform`.
+ * @param (Truth -> Truth) transform
+ */
 export function* chainTruthGenerator(truth, generator, transform) {
   const next = generator.next();
 
   if (!next.done) {
     const newTruth = transform(truth.bind(() => lifted(next.value)));
-    yield;
+    yield newTruth; // Let the user do one iteration at a time.
+
     yield* chainTruthGenerator(newTruth.bind(() => spoken("\n")),
         generator,
         transform);
@@ -14,6 +23,9 @@ export function* chainTruthGenerator(truth, generator, transform) {
   }
 }
 
+/**
+ * Returns an iterator over a range of integers.
+ */
 export function* range(start, end) {
   if (start <= end) {
     yield start;
